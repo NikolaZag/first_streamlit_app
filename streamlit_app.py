929ae29f89ba32c90422032e95c3af60fc2dc83e
NikolaZag <nikola.zagorski@gmail.com>
2023-02-01 11:15:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Bluebarry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
