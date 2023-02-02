8deee8f80874a0020ee64e59f1a1bd660c2e1a14
NikolaZag <nikola.zagorski@gmail.com>
2023-02-02 16:09:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-my.cur.execute("insert into fruit_load_list values('test');")
+my_cur.execute("insert into fruit_load_list values('from steamlit')")
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-my.cur.execute("insert into fruit_load_list values('test');")
+my_cur.execute("insert into fruit_load_list values('from steamlit')")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -36,7 +36,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -47,4 +47,4 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to Add?')
 streamlit.write('Thankls for adding ', add_my_fruit)
-my.cur.execute("insert into fruit_load_list values('test');")
+my_cur.execute("insert into fruit_load_list values('from steamlit')")
