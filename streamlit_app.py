1799c33736e00e47f824d0e9ffef29774954af08
NikolaZag <nikola.zagorski@gmail.com>
2023-02-02 16:20:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,17 @@ my_cur.execute("insert into fruit_load_list values('from steamlit')")
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
-my_cur.execute("insert into fruit_load_list values('from steamlit')")
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error("Please Select a fruit to get information.")
+  else: 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+expect URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
