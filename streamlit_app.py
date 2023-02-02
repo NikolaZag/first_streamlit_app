b34bb2bde00ee3d70a37a35d046059bc9d879445
NikolaZag <nikola.zagorski@gmail.com>
2023-02-02 16:13:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered', fruit_choice)
 my_cur.execute("insert into fruit_load_list values('from steamlit')")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
